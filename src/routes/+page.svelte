da5f0d2156dc6e4ecab6a853ee0cf21969a0d1e0
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2025-03-19 05:03:09 +0200

    Ran npm run format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { browser } from '$app/environment';
 	import { onMount } from 'svelte';
-    import { PUBLIC_OBA_LOGO_URL, PUBLIC_OBA_REGION_NAME} from '$env/static/public'
+	import { PUBLIC_OBA_LOGO_URL, PUBLIC_OBA_REGION_NAME } from '$env/static/public';
 
 	let arrivalsAndDepartures = $state([]);
 	let StopCode = $state('Loading...');
