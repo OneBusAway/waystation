393b0d5a72acc1df7974fe69a96dc667dd14f68b
Sundaresan-Karunakaran <karuns@usi.ch>
2025-03-20 16:26:09 +0100

    Fixed linting issues

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,12 @@
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
 	let { children } = $props();
-	import { PUBLIC_OBA_LOGO_URL, PUBLIC_OBA_REGION_NAME } from '$env/static/public'; 
+	import { PUBLIC_OBA_LOGO_URL, PUBLIC_OBA_REGION_NAME } from '$env/static/public';
 </script>
 
 <!-- Dynamically add custom favicon -->
 <svelte:head>
-	<link rel="icon" type="image/png" href="{PUBLIC_OBA_LOGO_URL}" />
+	<link rel="icon" type="image/png" href={PUBLIC_OBA_LOGO_URL} />
 	<title>{PUBLIC_OBA_REGION_NAME}</title>
 </svelte:head>
 
