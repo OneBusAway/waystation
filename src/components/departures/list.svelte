70cdaff0500ba6d28c517dafaf79d0440f355cc8
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2025-06-08 16:09:11 +0300

    Simplified/Optimized Alerts' fetching approach

--- src/components/departures/list.svelte
+++ src/components/departures/list.svelte
@@ -1,13 +1,16 @@
 <script>
 	import { formatArrivalStatus } from '$lib/formatters';
+
+	import Alerts from '$components/alerts/alerts.svelte';
 	import Departure from '$components/departures/departure.svelte';
 
 	let { stopID } = $props();
 
 	let arrivalsAndDepartures = $state([]);
+	let situations = $state([]);
 	let loading = $state(true);
 
-	export async function fetchDepartures() {
+	export async function fetchDeparturesAndAlerts() {
 		loading = true;
 		try {
 			const response = await fetch(`/api/oba/arrivals-and-departures-for-stop/${stopID}`);
@@ -18,6 +21,7 @@
 				const uniqueId = `${dep.tripId ?? '0'}-${dep.stopId ?? '0'}-${Math.floor(Math.random() * 10000)}`;
 				return { ...dep, status, uniqueId };
 			});
+			situations = responseBody.data.references?.situations || [];
 		} catch (error) {
 			console.error('Error fetching departures:', error);
 			arrivalsAndDepartures = [];
@@ -27,22 +31,31 @@
 	}
 </script>
 
-<div class="flex-1 bg-gray-200 text-black">
-	{#if loading}
-		<div class="flex h-32 items-center justify-center">
-			<p class="text-xl text-gray-600">Loading departures...</p>
-		</div>
-	{:else if arrivalsAndDepartures.length > 0}
-		<div class="flex flex-col divide-y divide-gray-300">
-			{#each arrivalsAndDepartures as dep (dep.uniqueId)}
-				{#if dep.status}
-					<Departure {dep} />
-				{/if}
-			{/each}
-		</div>
-	{:else}
-		<div class="flex h-32 items-center justify-center">
-			<p class="text-xl text-gray-600">No departures available</p>
+<div class="flex flex-1 gap-4 overflow-hidden">
+	<div class="flex flex-1 flex-col overflow-y-auto bg-gray-200 text-black">
+		{#if loading}
+			<div class="flex h-32 items-center justify-center">
+				<p class="text-xl text-gray-600">Loading departures...</p>
+			</div>
+		{:else if arrivalsAndDepartures.length > 0}
+			<div class="flex flex-col divide-y divide-gray-300">
+				{#each arrivalsAndDepartures as dep (dep.uniqueId)}
+					{#if dep.status}
+						<Departure {dep} />
+					{/if}
+				{/each}
+			</div>
+		{:else}
+			<div class="flex h-32 items-center justify-center">
+				<p class="text-xl text-gray-600">No departures available</p>
+			</div>
+		{/if}
+	</div>
+	{#if situations.length > 0}
+		<div class="w-[1px] bg-gray-300"></div>
+
+		<div class="w-[600px] flex-shrink-0 overflow-y-auto">
+			<Alerts {situations} />
 		</div>
 	{/if}
 </div>
--- src/routes/stops/[stopID]/+page.svelte
+++ src/routes/stops/[stopID]/+page.svelte
@@ -1,30 +1,30 @@
 <script>
 	import { PUBLIC_OBA_LOGO_URL, PUBLIC_OBA_REGION_NAME } from '$env/static/public';
-	import Alerts from '$components/alerts/alerts.svelte';
+
 	import Header from '$components/navigation/header.svelte';
 	import Footer from '$components/navigation/footer.svelte';
 	import DepartureList from '$components/departures/list.svelte';
 
-	import { situationsStore, fetchSituations } from '$lib/situations';
 	import { onMount, onDestroy } from 'svelte';
 	import { browser } from '$app/environment';
 
 	let { data } = $props();
-	const stop = data.stopData.entry;
 
-	let interval, departureList;
-	let hasAlerts = $derived($situationsStore.length > 0);
+	const stop = data.stopData.entry;
+	const REFRESH_INTERVAL = 30000; // todo: make it  a configurable option
+
+	let interval;
+	let departureList;
 
 	async function autoRefresh() {
-		await departureList.fetchDepartures();
-		await fetchSituations(data.stopID);
+		await departureList.fetchDeparturesAndAlerts();
 	}
 
 	onMount(async () => {
 		await autoRefresh();
 
 		if (browser) {
-			interval = setInterval(autoRefresh, 30000);
+			interval = setInterval(autoRefresh, REFRESH_INTERVAL);
 		}
 	});
 
@@ -39,14 +39,6 @@
 			<Header title={PUBLIC_OBA_REGION_NAME} imageUrl={PUBLIC_OBA_LOGO_URL} />
 			<DepartureList bind:this={departureList} stopID={data.stopID} />
 		</div>
-
-		{#if hasAlerts}
-			<div class="w-[1px] bg-gray-300"></div>
-
-			<div class="w-[600px] flex-shrink-0 overflow-y-auto">
-				<Alerts stopID={data.stopID} />
-			</div>
-		{/if}
 	</div>
 
 	<Footer {stop} class="shrink-0" />
