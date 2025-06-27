e85fec7858a7497bd73ede7b2f6ea65e8f92ccee
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2025-06-27 05:40:33 +0300

    Improved Screen Readability on large screens
    - Adjusted typograph for 1920x1080 viewports
    - Increased font sizes & boldness for better visibility

--- src/components/departures/departure.svelte
+++ src/components/departures/departure.svelte
@@ -10,21 +10,21 @@
 	const COLOR_CLASSES = {
 		green: 'text-green-500',
 		red: 'text-red-500',
-		blue: 'text-[#0095FF]',
-		gray: 'text-gray-500'
+		blue: 'text-[#0087E8]',
+		gray: 'text-[#8D8D8D]'
 	};
 
 	const BORDER_COLORS = {
 		green: 'border-[#00BD2F]',
 		red: 'border-[#EB3223]',
-		blue: 'border-[#0095FF]',
+		blue: 'border-[#0087E8]',
 		default: 'border-[#00273B66]'
 	};
 
 	const SHADOW_COLORS = {
 		green: 'rgba(0,189,47,0.1)',
 		red: 'rgba(235,50,35,0.03)',
-		blue: 'rgba(0,149,255,0.03)',
+		blue: 'rgba(0,149,255,0.07)',
 		default: 'rgba(0,0,0,0.08)'
 	};
 
@@ -42,21 +42,21 @@
 </script>
 
 <div
-	class={`m-4 flex items-center rounded-[16px] border-r-5 border-l-5 ${borderColor} bg-[rgba(255,255,255,0.85)] p-5`}
+	class={`m-4 flex items-center rounded-[32px] border-r-7 border-l-7 ${borderColor} bg-[rgba(255,255,255,0.85)] p-5`}
 	style={`box-shadow: 0 0 10px 10px ${shadowColor}`}
 >
 	<div
-		class="mr-5 flex min-w-28 items-center justify-center rounded-lg bg-[#00273B] px-4 py-3 text-2xl font-bold whitespace-nowrap text-white"
+		class="mr-5 flex min-w-42 items-center justify-center rounded-[20px] bg-[#00273B] px-7 py-6 text-4xl font-black whitespace-nowrap text-white"
 	>
 		{dep.routeShortName}
 	</div>
 
 	<div
-		class="flex min-w-0 flex-1 flex-wrap items-center gap-x-2 overflow-hidden text-xl leading-tight"
+		class="flex min-w-0 flex-1 flex-wrap items-center gap-x-2 overflow-hidden text-4xl leading-tight"
 	>
-		<span class="truncate font-semibold whitespace-nowrap">{dep.tripHeadsign}</span>
-		<span class="flex min-w-0 items-center gap-x-2 overflow-hidden whitespace-nowrap">
-			<ChevronRight class="text-gray-400" />
+		<span class="truncate font-extrabold whitespace-nowrap">{dep.tripHeadsign}</span>
+		<span class="flex min-w-0 items-center gap-x-2 overflow-hidden font-semibold whitespace-nowrap">
+			<ChevronRight class="text-gray-400" size={45} />
 			<span class="truncate">{dep.stopName}</span>
 		</span>
 	</div>
@@ -64,30 +64,30 @@
 	<div class="ml-3 flex items-center gap-1">
 		{#if status.status === 'Departing'}
 			<div class="flex items-center">
-				<ArrowUpLeft strokeWidth={2.3} size={28} />
-				<span class="ml-1 text-xl font-bold whitespace-nowrap">{status.text}</span>
+				<ArrowUpLeft strokeWidth={2.3} size={42} />
+				<span class="ml-3 text-4xl font-bold whitespace-nowrap">{status.text}</span>
 			</div>
 		{:else if status.minutes !== null}
-			<div class="flex items-center gap-1.5 whitespace-nowrap">
-				<ArrowDownRight class={colorClass} strokeWidth={2.3} size={28} />
-				<span class="text-lg whitespace-nowrap">{status.text}</span>
-				<span class={`${COLOR_CLASSES[routeStatus.color]} text-3xl font-bold`}
+			<div class="flex items-center gap-3 font-semibold whitespace-nowrap">
+				<ArrowDownRight class={colorClass} strokeWidth={2.3} size={42} />
+				<span class="text-4xl whitespace-nowrap">{status.text}</span>
+				<span class={`${COLOR_CLASSES[routeStatus.color]} text-5xl font-bold`}
 					>{status.minutes}</span
 				>
-				<span class="text-lg">min</span>
+				<span class="text-4xl">min</span>
 			</div>
 		{/if}
 
 		{#if status.status === 'Arriving' && (routeStatus.status || status.status === 'Scheduled')}
-			<ChevronRight class="text-gray-400" />
+			<ChevronRight size={36} class="text-gray-400" />
 		{/if}
 
 		<div class="flex flex-col items-end">
 			{#if status.status === 'Scheduled'}
-				<span class="text-4xl font-bold whitespace-nowrap">{status.displayTime}</span>
+				<span class="text-6xl font-bold whitespace-nowrap">{status.displayTime}</span>
 			{/if}
 			{#if routeStatus.status && status.status !== 'Departing'}
-				<span class={`${COLOR_CLASSES[routeStatus.color]} text-lg font-medium`}>
+				<span class={`${COLOR_CLASSES[routeStatus.color]} text-3xl font-bold`}>
 					{routeStatus.status}
 				</span>
 			{/if}
