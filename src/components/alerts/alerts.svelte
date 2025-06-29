e5d3816e81178a534dc14fc09f56a1358653e275
Amr Abdelfattah <amrabdelfattah321@gmail.com>
2025-06-29 06:01:05 +0300

    Used preset brand colors from @theme variables

--- src/components/alerts/alerts.svelte
+++ src/components/alerts/alerts.svelte
@@ -10,17 +10,15 @@
 
 	const dateStart = formatTimestamp(activeWindow.from ?? '');
 	const dateEnd = formatTimestamp(activeWindow.to ?? '');
-
-	const gray_bg = 'bg-[rgba(0,39,59,0.05)] font-bold px-5.5 py-1.5 corner rounded-[24px]';
 </script>
 
 {#if title}
 	<div class="m-5">
 		<div
-			class={`${gray_bg} bg-white py-5.5 text-[#003956]`}
+			class="text-brand-darkerblue corner rounded-[24px] bg-white px-5.5 py-8 font-bold"
 			style="box-shadow: 0 0 20px 1px rgba(0,0,0,0.15);"
 		>
-			<div class="flex items-center justify-center text-4xl font-bold text-[#FF5E51]">
+			<div class="text-brand-red flex items-center justify-center text-4xl font-bold">
 				Your attention required!
 			</div>
 
@@ -29,11 +27,12 @@
 			</div>
 
 			{#if validateTimestamp(dateStart) && validateTimestamp(dateEnd)}
-				<div class={`${gray_bg} flex items-center justify-center text-3xl`}>
-					<span class="whitespace-nowrap"
-						><span class="text-[#4BA12C]">{dateStart}</span> ➔
-						<span class="text-[#4BA12C]">{dateEnd}</span></span
-					>
+				<div class="flex items-center justify-center text-3xl">
+					<span class="whitespace-nowrap">
+						<span class="text-oba-green">{dateStart}</span>
+						➔
+						<span class="text-oba-green">{dateEnd}</span>
+					</span>
 				</div>
 			{/if}
 		</div>
