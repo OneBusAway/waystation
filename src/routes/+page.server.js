b0c8b7bb98d2fa407d4c74d2a37b7be3bf68c0b8
Aaron Brethorst <aaron@brethorsting.com>
2026-05-13 13:44:29 -0700

    feat: load stop IDs server-side when agency query param is present

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,12 +1,22 @@
 import oba, { handleOBAResponse } from '$lib/obaSdk.js';
 
-export async function load() {
+export async function load({ url }) {
 	const agencies = await oba.agenciesWithCoverage.list();
 	const agenciesBody = await handleOBAResponse(agencies, 'agencies').json();
-
 	const agencyRefs = agenciesBody.data.references.agencies;
 
+	const selectedAgencyId = url.searchParams.get('agency');
+	let stopIDs = [];
+
+	if (selectedAgencyId) {
+		const response = await oba.stopIdsForAgency.list(selectedAgencyId);
+		const json = await handleOBAResponse(response, 'stop-ids').json();
+		stopIDs = json.data.list;
+	}
+
 	return {
-		agencies: agencyRefs
+		agencies: agencyRefs,
+		selectedAgencyId,
+		stopIDs,
 	};
 }
